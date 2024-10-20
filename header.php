46aac63d8ef4078f2493a25c1e16e5d79e1b3e86
Ga-Sh-24 <128577525+Ga-Sh-24@users.noreply.github.com>
2024-10-20 19:00:58 +0530

    Update header.php

--- header.php
+++ header.php
@@ -3,11 +3,11 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     <style type="text/css">
         .brand{
-            background: #cbb09c !important;    /*!important is used for overriding any inbuilt class of materialize*/
+            background: #c32148 !important;    /*!important is used for overriding any inbuilt class of materialize*/
         }
 
         .brand-text{
-            color: #cbb09c !important;
+            color: #c32148 !important;
         }
 
         form{
@@ -19,19 +19,19 @@
         .pizza{
             width: 150px;
             height: 150px;
-            margin: 40px auto -30px;
+            margin: 60px auto -55px;
             display: block;
             position: relative;
-            top: -30px;
+            top: -35px;
         }
     </style>
 </head>
-<body class="grey lighten-2">
+<body class="grey lighten-3">
     <nav class="white z-depth-0">    
         <div class="container">    
             <a href="#" class="brand-logo brand-text">Sizzling Pizzas</a> 
             <ul id="nav-mobile" class="right hide-on-small-and-down">
-                <li class="grey-text">Hello Guest!</li>
+                <li class="grey-text text-darken-2"><strong>Hello Guest!</strong></li>
                 <li><a href="add.php" class="btn brand z-depth-0">Add a Pizza</a></li>
             </ul>
         </div>
